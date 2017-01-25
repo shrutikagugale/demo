ef1ec5b49bfeb9e33cae36c7d1572fcb986d923d
shrutika gugale <gugaleshru123@gmail.com>
2017-01-25 12:34:17 -0500

    first commit

---
+++ demo1.go
@@ -0,0 +1,24 @@
+// demo1
+package main
+
+import (
+	"fmt"
+)
+
+const s = "shrutika"
+
+func main() {
+	fmt.Println("Hello World!")
+	var a, b int = 2, 3
+	fmt.Println(a, b)
+	var d = true
+	fmt.Println(d)
+	var e int
+	fmt.Println(e)
+	f := "shot"
+	fmt.Println(f)
+	fmt.Println(s)
+	var n = 500000000
+	const d1 = 3e20 / n
+	fmt.Println(d1)
+}
